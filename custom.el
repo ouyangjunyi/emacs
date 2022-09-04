89df1f2314e350a1bc47ef24ddec425818822248
ouyangjunyi <zldark@126.com>
2022-09-04 17:00:04 +0800

    2022_09_04_17_00_01

--- custom.el
+++ custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(org-agenda-files
-   '("~/study/project/markdown/inner/markdown_submodule/done_pool.org" "/root/study/project/markdown/inner/markdown_submodule/okr_scheme.org" "/root/study/project/markdown/inner/markdown_submodule/demand_pool.org"))
+   '("~/study/notes/inner/markdown_submodule/done_pool.org" "/home/oyjy/study/notes/inner/markdown_submodule/demand_pool.org" "/home/oyjy/study/notes/inner/markdown_submodule/okr_scheme.org"))
  '(org-safe-remote-resources
    '("\\`https://fniessen\\.github\\.io/org-html-themes/org/theme-bigblow\\.setup\\'" "\\`https://fniessen\\.github\\.io/org-html-themes/org/theme-readtheorg\\.setup\\'")))
 (custom-set-faces
