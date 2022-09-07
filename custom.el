0ea787df97d65344436175773204cbca70f932ea
ouyangjunyi <zldark@126.com>
2022-09-07 14:20:05 +0800

    2022_09_07_14_20_01

--- custom.el
+++ custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(org-agenda-files
-   '("~/study/notes/inner/markdown_submodule/sundry/language/cpp/effective_cpp/effective_cpp.org" "/home/oyjy/study/notes/inner/markdown_submodule/done_pool.org" "/home/oyjy/study/notes/inner/markdown_submodule/demand_pool.org" "/home/oyjy/study/notes/inner/markdown_submodule/okr_scheme.org"))
+   '("~/study/notes/inner/markdown_submodule/features/physics/os_hardware.org" "/home/oyjy/study/notes/inner/markdown_submodule/done_pool.org" "/home/oyjy/study/notes/inner/markdown_submodule/demand_pool.org" "/home/oyjy/study/notes/inner/markdown_submodule/okr_scheme.org"))
  '(org-safe-remote-resources
    '("\\`https://fniessen\\.github\\.io/org-html-themes/org/theme-bigblow\\.setup\\'" "\\`https://fniessen\\.github\\.io/org-html-themes/org/theme-readtheorg\\.setup\\'")))
 (custom-set-faces
