29f328e5392465ed9669b26dc5dc93667ff1081c
ouyangjunyi <zldark@126.com>
2022-09-07 16:00:04 +0800

    2022_09_07_16_00_01

--- custom.el
+++ custom.el
@@ -3,6 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("c4063322b5011829f7fdd7509979b5823e8eea2abf1fe5572ec4b7af1dd78519" "0466adb5554ea3055d0353d363832446cd8be7b799c39839f387abb631ea0995" default))
  '(org-agenda-files
    '("~/study/notes/inner/markdown_submodule/features/physics/os_hardware.org" "/home/oyjy/study/notes/inner/markdown_submodule/done_pool.org" "/home/oyjy/study/notes/inner/markdown_submodule/demand_pool.org" "/home/oyjy/study/notes/inner/markdown_submodule/okr_scheme.org"))
  '(org-safe-remote-resources
