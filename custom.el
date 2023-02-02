47073edda2b6548f9ca3ac873550377a6a531cb8
ouyangjunyi <zldark@126.com>
2023-02-02 14:57:05 +0800

    add org-drill

--- custom.el
+++ custom.el
@@ -8,7 +8,8 @@
  '(org-agenda-files
    '("~/study/notes/inner/markdown_submodule/features/physics/os_hardware.org" "/home/oyjy/study/notes/inner/markdown_submodule/done_pool.org" "/home/oyjy/study/notes/inner/markdown_submodule/demand_pool.org" "/home/oyjy/study/notes/inner/markdown_submodule/okr_scheme.org"))
  '(org-safe-remote-resources
-   '("\\`https://fniessen\\.github\\.io/org-html-themes/org/theme-bigblow\\.setup\\'" "\\`https://fniessen\\.github\\.io/org-html-themes/org/theme-readtheorg\\.setup\\'")))
+   '("\\`https://fniessen\\.github\\.io/org-html-themes/org/theme-bigblow\\.setup\\'" "\\`https://fniessen\\.github\\.io/org-html-themes/org/theme-readtheorg\\.setup\\'"))
+ '(package-selected-packages '(org-drill)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
